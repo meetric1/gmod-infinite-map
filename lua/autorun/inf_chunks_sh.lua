f246f74f9926d128a797bd164d6533651a425ca5
MyUsernamee <86261814+MyUsernamee@users.noreply.github.com>
2022-10-24 18:44:22 -0700

    Add Documentation
    Add some documentation

--- lua/autorun/inf_chunks_sh.lua
+++ lua/autorun/inf_chunks_sh.lua
@@ -4,6 +4,7 @@ AddCSLuaFile()
 
 InfMap = InfMap or {}
 
+-- Creates the should collide hook for managing collisions
 hook.Add("ShouldCollide", "inf_chunk", function(ent1, ent2)
 	return InfMap.should_collide(ent1, ent2)
 end)
