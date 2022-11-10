aa0a2af0fde090bcb9dea305af76365ce3833664
WFL <100780230+willfromlondon@users.noreply.github.com>
2022-11-10 11:11:08 +0000

    Update sv_inf_detours.lua

--- lua/infmap/server/sv_inf_detours.lua
+++ lua/infmap/server/sv_inf_detours.lua
@@ -311,10 +311,10 @@ hook.Add( "EntityEmitSound", "inf_ent_sound", function( t )
 	if t.Entity:IsWorld() then //emitting sounds from world is stupid, lets change to entity
 		local dist = 2^14
 		local ent, vec, co
-		for k, v in pairs( ents.GetAll() ) do //findinsphere more efficient, but don't know entity size
+		for k, v in pairs( ents.GetAll() ) do
 			vec = InfMap.localize_vector(v:GetPos()) //get true pos correction
-			if vec:DistToSqr(t.Pos) < dist and v:GetClass() != "infmap_terrain_collider" then //get closest entity to emitted sound, not terrain collider
-				dist = v:GetPos():DistToSqr(t.Pos)
+			if vec:DistToSqr(t.Pos) < dist and v:GetClass() != "infmap_terrain_collider" then //get closest entity to emitted sound, not terrain collider!
+				dist = vec:DistToSqr(t.Pos)
 				ent = v
 			end
 		end
