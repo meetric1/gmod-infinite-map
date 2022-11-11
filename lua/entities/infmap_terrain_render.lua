11f881c488ea5c746f1236fdbe5a25277b811a53
Mee12345 <73554599+Mee12345@users.noreply.github.com>
2022-11-10 23:34:51 -0500

    fix memory leak related to cleaning up map

--- lua/entities/infmap_terrain_render.lua
+++ lua/entities/infmap_terrain_render.lua
@@ -12,6 +12,8 @@ ENT.Purpose			= ""
 ENT.Instructions	= ""
 ENT.Spawnable		= false
 
+if SERVER then return end	// clientside only entity
+
 local uvscale = 150
 local ud = {1, 1, 1, 1}
 local function add_quad(tab, p1, p2, p3, p4, n1, n2)
@@ -148,95 +150,94 @@ function ENT:OnRemove()
 	end
 end
 
-if CLIENT then
-	local last_mega_chunk
-	InfMap.client_chunks = InfMap.client_chunks or {}
-	for y = -InfMap.render_distance, InfMap.render_distance do
-		InfMap.client_chunks[y] = InfMap.client_chunks[y] or {}
-	end
-    hook.Add("PropUpdateChunk", "infmap_terrain_init", function(ent, chunk, old_chunk)
-        if ent == LocalPlayer() then
-			local _, mega_chunk = InfMap.localize_vector(chunk, InfMap.megachunk_size) mega_chunk[3] = 0
-			local chunk_res_scale = InfMap.chunk_size * 2 * InfMap.megachunk_size * 2
-			local chunk_scale = InfMap.chunk_size * 2
-			local delta_chunk = mega_chunk - (last_mega_chunk or mega_chunk)
-			local chunk_alloc = table.Copy(InfMap.client_chunks)
-			local time = 0
-			for y = -InfMap.render_distance, InfMap.render_distance do
-				for x = -InfMap.render_distance, InfMap.render_distance do
-					// if the chunk the current xy chunk is going to go to is outside of the render distance remove it
-					if math.abs(x - delta_chunk[1]) > InfMap.render_distance or math.abs(y - delta_chunk[2]) > InfMap.render_distance then
-						SafeRemoveEntity(InfMap.client_chunks[y][x])
-						InfMap.client_chunks[y][x] = nil
-					end
 
-					if chunk_alloc[y + delta_chunk[2]] and chunk_alloc[y + delta_chunk[2]][x + delta_chunk[1]] then
-						InfMap.client_chunks[y][x] = chunk_alloc[y + delta_chunk[2]][x + delta_chunk[1]]
-					else
-						InfMap.client_chunks[y][x] = nil
-					end
-					// create chunk if it doesnt exist
-					if !IsValid(InfMap.client_chunks[y][x]) then 
-						local e = ents.CreateClientside("infmap_terrain_render")
-						e:Spawn()
-						e.CHUNK_OFFSET = Vector(x, y, 0) + mega_chunk
-						time = time + 0.01
-						e:GenerateMesh(InfMap.height_function, (Vector(x, y, 0) + mega_chunk) * InfMap.megachunk_size * 2, time)
-						InfMap.client_chunks[y][x] = e
-					end
-
-					local e = InfMap.client_chunks[y][x]
-					if !e.RENDER_MESH then continue end
-					e.RENDER_MESH.Matrix:SetTranslation(e.CHUNK_OFFSET * chunk_res_scale - chunk * chunk_scale)
-				end
-			end
-
-			last_mega_chunk = mega_chunk
-        end
-    end)
-
-	// update renderbounds for these entities
-	hook.Add("RenderScene", "infmap_update_renderbounds", function(eyePos)
+local last_mega_chunk
+InfMap.client_chunks = InfMap.client_chunks or {}
+for y = -InfMap.render_distance, InfMap.render_distance do
+	InfMap.client_chunks[y] = InfMap.client_chunks[y] or {}
+end
+hook.Add("PropUpdateChunk", "infmap_terrain_init", function(ent, chunk, old_chunk)
+	if ent == LocalPlayer() then
+		local _, mega_chunk = InfMap.localize_vector(chunk, InfMap.megachunk_size) mega_chunk[3] = 0
+		local chunk_res_scale = InfMap.chunk_size * 2 * InfMap.megachunk_size * 2
+		local chunk_scale = InfMap.chunk_size * 2
+		local delta_chunk = mega_chunk - (last_mega_chunk or mega_chunk)
+		local chunk_alloc = table.Copy(InfMap.client_chunks)
+		local time = 0
 		for y = -InfMap.render_distance, InfMap.render_distance do
 			for x = -InfMap.render_distance, InfMap.render_distance do
-				local chunk = InfMap.client_chunks[y][x]
-				if !IsValid(chunk) or !chunk.RENDER_MESH then continue end
-				chunk:SetLocalRenderBounds(eyePos)
+				// if the chunk the current xy chunk is going to go to is outside of the render distance remove it
+				if math.abs(x - delta_chunk[1]) > InfMap.render_distance or math.abs(y - delta_chunk[2]) > InfMap.render_distance then
+					SafeRemoveEntity(InfMap.client_chunks[y][x])
+					InfMap.client_chunks[y][x] = nil
+				end
+
+				if chunk_alloc[y + delta_chunk[2]] and chunk_alloc[y + delta_chunk[2]][x + delta_chunk[1]] then
+					InfMap.client_chunks[y][x] = chunk_alloc[y + delta_chunk[2]][x + delta_chunk[1]]
+				else
+					InfMap.client_chunks[y][x] = nil
+				end
+				// create chunk if it doesnt exist
+				if !IsValid(InfMap.client_chunks[y][x]) then 
+					local e = ents.CreateClientside("infmap_terrain_render")
+					e:Spawn()
+					e.CHUNK_OFFSET = Vector(x, y, 0) + mega_chunk
+					time = time + 0.01
+					e:GenerateMesh(InfMap.height_function, (Vector(x, y, 0) + mega_chunk) * InfMap.megachunk_size * 2, time)
+					InfMap.client_chunks[y][x] = e
+				end
+
+				local e = InfMap.client_chunks[y][x]
+				if !e.RENDER_MESH then continue end
+				e.RENDER_MESH.Matrix:SetTranslation(e.CHUNK_OFFSET * chunk_res_scale - chunk * chunk_scale)
 			end
 		end
-	end)
 
-	hook.Add("PreCleanupMap", "infmap_terrain_cleanup", function()
-		for y, t in pairs(InfMap.client_chunks) do
-			for x, ent in pairs(t) do
-				SafeRemoveEntity(ent)
-			end
+		last_mega_chunk = mega_chunk
+	end
+end)
+
+// update renderbounds for these entities
+hook.Add("RenderScene", "infmap_update_renderbounds", function(eyePos)
+	for y = -InfMap.render_distance, InfMap.render_distance do
+		for x = -InfMap.render_distance, InfMap.render_distance do
+			local chunk = InfMap.client_chunks[y][x]
+			if !IsValid(chunk) or !chunk.RENDER_MESH then continue end
+			chunk:SetLocalRenderBounds(eyePos)
 		end
-	end)
+	end
+end)
 
-	local size = 2^31
-	local uvsize = size / 10000
-	local min = -1000000
-	local data = {
-        {pos = Vector(size, size, min), normal = Vector(0, 0, 1), u = uvsize, v = 0, tangent = Vector(1, 0, 0), userdata = {1, 0, 0, -1}},
-        {pos = Vector(size, -size, min), normal = Vector(0, 0, 1), u = uvsize, v = uvsize, tangent = Vector(1, 0, 0), userdata = {1, 0, 0, -1}},
-        {pos = Vector(-size, -size, min), normal = Vector(0, 0, 1), u = 0, v = uvsize, tangent = Vector(1, 0, 0), userdata = {1, 0, 0, -1}},
-        {pos = Vector(size, size, min), normal = Vector(0, 0, 1), u = uvsize, v = 0, tangent = Vector(1, 0, 0), userdata = {1, 0, 0, -1}},
-        {pos = Vector(-size, -size, min), normal = Vector(0, 0, 1), u = 0, v = uvsize, tangent = Vector(1, 0, 0), userdata = {1, 0, 0, -1}},
-        {pos = Vector(-size, size, min), normal = Vector(0, 0, 1), u = 0, v = 0, tangent = Vector(1, 0, 0), userdata = {1, 0, 0, -1}},
-    }
+hook.Add("PostCleanupMap", "infmap_terrain_cleanup", function()
+	for y, t in pairs(InfMap.client_chunks) do
+		for x, ent in pairs(t) do
+			SafeRemoveEntity(ent)
+		end
+	end
+end)
 
-	local big_plane = Mesh()
-	big_plane:BuildFromTriangles(data)
-	hook.Add("PostDraw2DSkyBox", "infmap_terrain_drawover", function()
-		local mat = Matrix()
-		local lpp = LocalPlayer():GetPos()
-		mat:SetTranslation(-InfMap.unlocalize_vector(Vector(), Vector(0, 0, -10)))
-		render.OverrideDepthEnable(true, false)
-		render.SetMaterial(default_mat)
-		cam.PushModelMatrix(mat)
-		big_plane:Draw()
-		cam.PopModelMatrix()
-		render.OverrideDepthEnable(false, false)
-	end)
-end
+local size = 2^31
+local uvsize = size / 10000
+local min = -1000000
+local data = {
+	{pos = Vector(size, size, min), normal = Vector(0, 0, 1), u = uvsize, v = 0, tangent = Vector(1, 0, 0), userdata = {1, 0, 0, -1}},
+	{pos = Vector(size, -size, min), normal = Vector(0, 0, 1), u = uvsize, v = uvsize, tangent = Vector(1, 0, 0), userdata = {1, 0, 0, -1}},
+	{pos = Vector(-size, -size, min), normal = Vector(0, 0, 1), u = 0, v = uvsize, tangent = Vector(1, 0, 0), userdata = {1, 0, 0, -1}},
+	{pos = Vector(size, size, min), normal = Vector(0, 0, 1), u = uvsize, v = 0, tangent = Vector(1, 0, 0), userdata = {1, 0, 0, -1}},
+	{pos = Vector(-size, -size, min), normal = Vector(0, 0, 1), u = 0, v = uvsize, tangent = Vector(1, 0, 0), userdata = {1, 0, 0, -1}},
+	{pos = Vector(-size, size, min), normal = Vector(0, 0, 1), u = 0, v = 0, tangent = Vector(1, 0, 0), userdata = {1, 0, 0, -1}},
+}
+
+local big_plane = Mesh()
+big_plane:BuildFromTriangles(data)
+hook.Add("PostDraw2DSkyBox", "infmap_terrain_drawover", function()
+	local mat = Matrix()
+	local lpp = LocalPlayer():GetPos()
+	mat:SetTranslation(-InfMap.unlocalize_vector(Vector(), Vector(0, 0, -10)))
+	render.OverrideDepthEnable(true, false)
+	render.SetMaterial(default_mat)
+	cam.PushModelMatrix(mat)
+	big_plane:Draw()
+	cam.PopModelMatrix()
+	render.OverrideDepthEnable(false, false)
+end)
