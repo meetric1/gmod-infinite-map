535475896cfd03f24e89ac655f18961fe44d4a3c
pizzamagician <60462699+pizzamagician@users.noreply.github.com>
2022-12-04 00:28:35 +0300

    lmao

--- lua/infmap/client/cl_inf_clouds.lua
+++ lua/infmap/client/cl_inf_clouds.lua
@@ -4,8 +4,8 @@ local size = 50000000 //terrain blinks after size being more than 50000000
 hook.Add( "PostDrawTranslucentRenderables", "DrawClouds", function()
     render.SetMaterial( mat )
     
-    local vec = Vector(LocalPlayer().CHUNK_OFFSET[1],LocalPlayer().CHUNK_OFFSET[2],-LocalPlayer().CHUNK_OFFSET[3]) //immovable object, unstoppable force
-
+    local vec = Vector(LocalPlayer().CHUNK_OFFSET)
+    vec[3] = -vec[3]
+    
     render.DrawQuadEasy( InfMap.unlocalize_vector(Vector(0,0,500000), vec), Vector(0,0,-1), size, size, Color(255,255,255) )
-    render.DrawQuadEasy( InfMap.unlocalize_vector(Vector(0,0,500000), vec), Vector(0,0,1), size, size, Color(255,255,255) ) //because it's a plane
 end)
