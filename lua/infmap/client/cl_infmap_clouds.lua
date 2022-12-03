6c42111860a670f595498ed92eec7ceaef2767e0
pizzamagician <60462699+pizzamagician@users.noreply.github.com>
2022-12-03 21:21:57 +0300

    Delete cl_infmap_clouds.lua

--- lua/infmap/client/cl_infmap_clouds.lua
+++
@@ -1,8 +0,0 @@
-local mat = Material( "infmap/atmosphere" )
-local size = 50000000 //terrain blinks after size being more than 50000000
-
-hook.Add( "PostDrawTranslucentRenderables", "DrawClouds", function()
-    render.SetMaterial( mat )
-    render.DrawQuadEasy( InfMap.unlocalize_vector(Vector(0,0,500000), -LocalPlayer().CHUNK_OFFSET), Vector(0,0,-1), size, size, Color(255,255,255) )
-    render.DrawQuadEasy( InfMap.unlocalize_vector(Vector(0,0,500000), -LocalPlayer().CHUNK_OFFSET), Vector(0,0,1), size, size, Color(255,255,255) ) //because it's a plane
-end)
